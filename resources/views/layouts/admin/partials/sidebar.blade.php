8340025d9185530177399e56682e5399ba3f1887
Didi Abdillah Arvidev <72367743+didiabdillah@users.noreply.github.com>
2025-08-02 17:00:32 +0700

    feat(ui): add custom logo assets and update branding across layouts
    - Add three new logo variants (logo-1.png, logo-2.png, logo-3.png) to assets
    - Replace AdminLTE default logo with custom logo-2.png in admin sidebar and public navbar
    - Remove img-circle class from logo elements for better visual presentation
    - Update footer copyright link to point to application home instead of AdminLTE.io

--- resources/views/layouts/admin/partials/sidebar.blade.php
+++ resources/views/layouts/admin/partials/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/') }}" class="brand-link">
-        <img src="{{ asset('assets/img/AdminLTELogo.png') }}"
+        <img src="{{ asset('assets/img/logo/logo-2.png') }}"
              alt="Logo"
-             class="brand-image img-circle elevation-3"
+             class="brand-image elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
     </a>
--- resources/views/layouts/public/partials/navbar.blade.php
+++ resources/views/layouts/public/partials/navbar.blade.php
@@ -2,7 +2,7 @@
   <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
     <div class="container">
       <a href="" class="navbar-brand">
-        <img src="{{asset('assets/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="{{asset('assets/img/logo/logo-2.png')}}" alt="Logo" class="brand-image elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">{{ config('app.name', 'Pinjam Arsip') }}</span>
       </a>
 
